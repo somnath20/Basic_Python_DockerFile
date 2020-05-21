59ab7da80bbce3028617477fdd91bf9be04bf42a
Somnath Paul <somnath.20.paul@gmail.com>
2020-05-21 22:35:58 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,23 @@
 from flask import Flask
 app = Flask(__name__)
+
 @app.route("/")
 def hello():
     return "Hello World!"
+
+@app.route('/index')
+def index():
+    user = {'username': 'Somnath'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
